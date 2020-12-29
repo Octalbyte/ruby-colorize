7a796b4f8e569d2c883ca90af71e61ffdac39241
JPSO <66487668+J-P-S-O@users.noreply.github.com>
2020-12-29 10:04:28 -0300

    Update core.rb

--- core.rb
+++ core.rb
@@ -6,3 +6,15 @@ def colorize.red(str)
    str = red + str + reset
    return str
 end
+def colorize.blue(str)
+   blue = "\x1b[34m"
+   reset = "\x1b[0m"
+   str = red + str + reset
+   return str
+end
+def colorize.green(str)
+   green = "\x1b[32m"
+   reset = "\x1b[0m"
+   str = red + str + reset
+   return str
+end
