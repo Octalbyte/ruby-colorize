6e67b929077ff03622867825ad54732daa0ba141
JPSO <66487668+J-P-S-O@users.noreply.github.com>
2020-12-31 11:13:08 -0300

    Update core.rb

--- core.rb
+++ core.rb
@@ -1,39 +1,50 @@
 
 # https://stackoverflow.com/questions/9781218/how-to-change-node-jss-console-font-color
 colorize = {}
+
 def colorize.red(str)
    red = "\x1b[31m"
    reset = "\x1b[0m"
    str = red + str + reset
    return str
 end
+
 def colorize.blue(str)
    blue = "\x1b[34m"
    reset = "\x1b[0m"
    str = blue + str + reset
    return str
 end
+
 def colorize.green(str)
    green = "\x1b[32m"
    reset = "\x1b[0m"
    str = green + str + reset
    return str
 end
+
+
 def colorize.yellow(str)
    yellow = "\x1b[33m"
    reset = "\x1b[0m"
    str = yellow + str + reset
    return str
 end
+
+
 def colorize.cyan(str)
    cyan = "\x1b[36m"
    reset = "\x1b[0m"
    str = cyan + str + reset
    return str
 end
+
+
 def colorize.magenta(str)
    magenta = "\x1b[35m"
    reset = "\x1b[0m"
    str = magenta + str + reset
    return str
 end
+
+
